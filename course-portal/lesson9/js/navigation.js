7519482add6d551cb184f87b9937a6ea995a83b5
junniepat <patrickoigwe@gmail.com>
2021-03-03 16:09:19 +0400

    prophets

---
+++ course-portal/lesson9/js/navigation.js
@@ -0,0 +1,15 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
---
+++ course-portal/lesson9/js/prophets.js
@@ -0,0 +1,33 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let image =document.createElement('img');
+        let p = document.createElement('p');
+
+
+        image.setAttribute('src', prophets[i].imageurl);
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + ' ' + 'Death:' + '' + prophets[i].death + ' ' + 'Length:' + '' + prophets[i].length + ' ' +
+        'Order:' + ' ' + prophets[i].order + ' ' + 'Birth place:' + ' ' + prophets[i].birthplace + ' ' + 'Children:' + ' ' + prophets[i].numofchildren;
+
+
+        card.appendChild(image);
+        card.appendChild(h2);
+        card.appendChild(p);
+
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+  });
+  
+
+
