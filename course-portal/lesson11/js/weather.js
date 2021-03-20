84556884d0ad5ee0d4fdf3a958d5420cfdeb9073
junniepat <patrickoigwe@gmail.com>
2021-03-21 02:49:08 +0400

    api

--- course-portal/lesson11/js/weather.js
+++ course-portal/lesson11/js/weather.js
@@ -1,3 +1,46 @@
+
+
+
+// PRESTON EVENT 
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+        towns.forEach(town => {
+            if (town.name == "Preston"){
+                let event = document.createElement('div')
+                let div = document.createElement('div')
+ 
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+
+                div.setAttribute('class', 'eventInfo')
+                p1.textContent = town.events[0];
+                p2.textContent = town.events[1];
+                p3.textContent = town.events[2];
+                p4.textContent = town.events[3];
+
+
+                div.appendChild(p1);
+                div.appendChild(p2);
+                div.appendChild(p3);
+                div.appendChild(p4);
+                event.appendChild(div);
+
+                document.querySelector('div.EventsItems').appendChild(event);
+            }
+        });
+
+    });
+
+
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=4720583&units=imperial&appid=db10eaf58a4d0a2fae9355c262a4613e';
 fetch(apiURL)
   .then((response) => response.json())
